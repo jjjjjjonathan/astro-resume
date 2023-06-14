5c76f3437144ab617d3845a394a9b3215ae2539a
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2023-06-14 15:10:15 -0400

    Uninstall vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from 'astro-icon';
 
-import vue from "@astrojs/vue";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     config: {
       applyBaseStyles: false
     }
-  }), vue(), icon({
+  }), icon({
     iconDir: 'src/icons',
     include: {
       mdi: ['email', 'earth', 'map-marker-radius', 'github', 'linkedin', 'phone-classic', 'link-variant', 'star-check']
