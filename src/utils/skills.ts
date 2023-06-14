c2b40bbc7bd6c330e372b681dcc4c43ecb52423a
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2023-06-14 14:25:35 -0400

    Display skills

---
+++ src/utils/skills.ts
@@ -0,0 +1,8 @@
+import { skills } from './resume.json';
+
+export const getSkills = () => {
+  return skills.map((skillSection) => ({
+    name: skillSection.name,
+    tags: skillSection.keywords,
+  }))
+};
