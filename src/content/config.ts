bf39ee2b467aa7f701918beb69b252867ca1b8e1
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2023-06-16 15:32:22 -0400

    Add example cover letter

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { z, defineCollection } from "astro:content";
+
+const coverLetterCollection = defineCollection({
+  schema: z.object({
+    date: z.date().optional(),
+    company: z.string(),
+    address: z.string(),
+    city: z.string(),
+    province: z.string(),
+    postalCode: z.string(),
+    position: z.string(),
+  }),
+});
+
+export const collections = {
+  coverletters: coverLetterCollection,
+};
